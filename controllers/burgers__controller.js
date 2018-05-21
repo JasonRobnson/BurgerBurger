b8fb6fdfc9eb68c9f074d512b346d5e3175c6809
unknown <bubbastinx@gmail.com>
2018-05-20 19:10:47 -0600

    converted newBurger variable to an object instead of a variable, able to drill down into req.body now

--- controllers/burgers__controller.js
+++ controllers/burgers__controller.js
@@ -3,28 +3,17 @@ let router = express.Router();
 let burger = require("../models/burger.js")
 
 router.get("/", (req, res ) => {
-    // res.send("This is from the Root route!")
-    console.log("foobar")
     burger.all("burgers", (data) => {
         let HandbarsObj = {
         burgers: data
         };
-        console.log(HandbarsObj);
-        console.log("!!!!!!")
+      
         res.render("index",HandbarsObj);
     });
 });
 
 router.post("/api/burgers", (req, res) => {
-    console.log(req.body)
-    // burger.create([
-    //    console.log(req.body.newBurger)
-    // ], (result) => {
-    //     console.log("Ring Ring!")
-    // })
-    // console.log("API ")
-    // res.send("This is the api/burger ROUTE");
-   
+        console.log(req.body.burger_name)
 })
 
 
